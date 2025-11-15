ac58f7fadce8e1fa0bee5a9f836a26353efb22a0
mamdadDev2024 <madmanprogrammer2024@gmail.com>
2025-11-15 16:52:56 +0330

    - add gitignore file to public folder - remove test image background from app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -28,13 +28,9 @@
 
 <body class="bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-100 min-h-flex min-w-full flex-col transition-colors duration-300 relative">
 
-    <!-- Background Animal Image -->
     <div class="fixed inset-0 z-0 overflow-hidden pointer-events-none">
         <div class="absolute inset-0 bg-gradient-to-br from-gray-50/50 via-white/30 to-gray-100/50 dark:from-gray-900/50 dark:via-gray-950/30 dark:to-gray-900/50"></div>
-        <img src="https://www.beytoote.com/images/stories/fun/hhf63.jpg" 
-             alt="Animal background" 
-             class="absolute inset-0 w-full h-full object-cover opacity-10 dark:opacity-5 blur-sm"
-             style="object-position: center;">
+
     </div>
 
     <div class="relative z-10">
