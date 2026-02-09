132798728a50fce755b77fac8eb55cf1910ce83f
Mad Man <madmanprogrammer2024@gmail.com>
2026-02-09 18:04:54 +0330

    delete extra routes

--- Modules/Interaction/routes/web.php
+++ Modules/Interaction/routes/web.php
@@ -2,8 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-    Route::prefix('comment')->as('comment.')->group(function () {
-        Route::get('', \Modules\Interaction\Livewire\CommentIndex::class)->name('index');
-    });
-});
+Route::middleware('auth')->group(function () {});
--- Modules/Media/routes/web.php
+++ Modules/Media/routes/web.php
@@ -2,10 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Media\Http\Controllers\DownloadController;
-use Modules\Media\Http\Controllers\MediaController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('media', MediaController::class)->names('media');
-});
 
 Route::get('/media/view/{id}', DownloadController::class)->middleware('throttle:media-view')->name('download');
